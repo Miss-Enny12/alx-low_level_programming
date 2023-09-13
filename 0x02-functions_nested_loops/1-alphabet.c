b87bba61329d53597c5d4b7c93c7536890d3c67f
Miss-Enny12 <eniolaolayeni01@gmail.com>
2023-09-13 13:07:38 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - This prints alphabets in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i = 'z'; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
