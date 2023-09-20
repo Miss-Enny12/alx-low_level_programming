c9e887c17811da60ac541496493dd69f27e388d7
Miss-Enny12 <eniolaolayeni01@gmail.com>
2023-09-19 19:16:39 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * _strcpy - function name
+ * @dest: function parameter
+ * @src: parameter 2
+ * Return: always 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+{
+	dest[a] = src[a];
+}
+	dest[a] = '\0';
+	return (dest);
+}
