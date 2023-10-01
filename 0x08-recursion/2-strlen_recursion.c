4e6c587d6daa1e71acfcf2ba00a2174b775805db
Miss-Enny12 <eniolaolayeni01@gmail.com>
2023-10-01 13:02:52 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,1 +1,17 @@
-#include
+#include "main.h"
+/**
+ * _strlen_recursion - main function
+ * @s: function parameter
+ * Return: Length
+ */
+int _strlen_recursion(char *s)
+{
+	int Int = 0;
+
+	if (*s)
+{
+	Int++;
+	Int += _strlen_recursion(s + 1);
+}
+	return (Int);
+}
