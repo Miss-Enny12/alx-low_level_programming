bc36ba5aa9a77683b25008783422b3dcbf2d80f0
Miss-Enny12 <eniolaolayeni01@gmail.com>
2023-10-01 12:17:04 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _puts_recursion - main function
+ * @s: function parameter
+ * Return: alaways 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+
+{
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
+	else
+	putchar('\n');
+}
